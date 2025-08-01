d91542838affc7b802528051a7fb8a28ac11adb6
Scott Walter <scottwalter69@gmail.com>
2025-08-01 18:27:09 -0400

    save

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -5,10 +5,19 @@ const fetch = require('node-fetch');
 
 const apiUrlBitAxe1 = 'http://192.168.7.220/api/system/info';
 const apiUrlBitAxe2 = 'http://192.168.7.122/api/system/info';
+const apiPath = '/api/system/info';
 
 async function display (req,res,config){
 try {
-  console.log(`Config: ${config.bitaxe_instances}`);
+    //new method
+    let tables ='<table border="1"><tr>';
+    tables += await callWebService(config);
+    //Close up the master table
+    tables += '</tr></table>';
+    console.log(`Table: ${tables}`);
+    //End new method
+      console.log(`Config: ${config.bitaxe_instances}`);
+     /**
       //Pull Data from BitAxe1
       const response1 = await fetch(apiUrlBitAxe1);
       const data1 = await response1.json();
@@ -31,13 +40,14 @@ try {
       let tableHtmlTotal ='<table border="1">';
       tableHtmlTotal +=`<tr><td>${tableHtml1}</td><td>${tableHtml2}</td></tr>`;
       tableHtmlTotal+='</table>';
+      */
       // Display master table on web page
       fs.readFile(path.join(__dirname, 'dashboard.html'), 'utf8', (err, htmlContent) => {
         if (err) {
           res.writeHead(500, { 'Content-Type': 'text/plain' });
           res.end('Error loading index.html');
         } else {
-          const finalHtml = htmlContent.replace('<!-- DATA_TABLE -->', tableHtmlTotal);
+          const finalHtml = htmlContent.replace('<!-- DATA_TABLE -->', tables);
           res.writeHead(200, { 'Content-Type': 'text/html' });
           res.end(finalHtml);
         }
@@ -50,6 +60,27 @@ try {
     }
     return '1';
   };
+  async function callWebService(config){
+    let tables;
+    config.bitaxe_instances.forEach((instance, index) =>{
+        //Fetch the data for this instance
+        const instanceName = Object.keys(instance)[0];
+        const instanceUrl = instance[instanceName];
+        console.log(`About to fetch: ${instanceName} ${instanceUrl} ${apiPath}`);
+        //const response = await fetch(instanceUrl+apiPath);
+        const response = await fetch(instanceUrl+apiPath);
+        const data =  await response.json();
+        console.log(`Fecthed: ${data}`);
+        tables +='<td><table border="1">';
+        for (const key in data){
+          console.log(`reading key ${key} with value ${data[key]}`);
+          tables +=`<tr><td>${key}</td><td>${data[key]}</td></tr>`;
+        }
+        tables +='</table></td>';
+    });
+    return tables;
+    
+  }
 module.exports ={
     display
 }
